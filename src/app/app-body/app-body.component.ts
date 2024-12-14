675cad5b40896ac2a4ace777b3ad9937c8f9b7a5
Mouhamad_Kraytem <moohamadkraytem15@gmail.com>
2024-12-14 21:19:50 +0200

    first commit

---
+++ src/app/app-body/app-body.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-body',
+  imports: [],
+  templateUrl: './app-body.component.html',
+  styleUrl: './app-body.component.css'
+})
+export class AppBodyComponent {
+
+}
